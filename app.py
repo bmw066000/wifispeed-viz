5c94f4e4509c10b2728e3f8973a204d461974024
Fred Exley <bmw066000@gmail.com>
2016-10-01 10:46:32 -0500

    the actual data...

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
+from subprocess import Popen, PIPE
 import json
-import random
 import datetime
+import csv
+import sys
+import time
+import copy
+import os
 
 app = Flask(__name__)
 
@@ -32,8 +37,26 @@ def kill():
 @app.route('/get_data')
 def get_data():
     global start_time
-    speed = random.randint(100, 200)
-    return json.dumps([(datetime.datetime.now() - start_time).seconds, speed])
+    ON_POSIX = 'posix' in sys.builtin_module_names
+
+    log_path = "./speed_log.csv"
+    if not os.path.isfile(log_path):
+        with open(log_path, 'w') as log_file:
+            writer = csv.writer(log_file)
+            writer.writerow(['time', 'ping', 'download', 'upload'])
+
+    p = Popen(['/anaconda/bin/speedtest-cli', '--simple', '--secure'], stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
+    output, error = p.communicate()
+    data_entry = [float(line.strip().split()[1]) for line in output.splitlines()]
+    data_entry.insert(0, (datetime.datetime.now() - start_time).seconds)
+    retval = copy.deepcopy(data_entry)
+    print(data_entry)
+    data_entry[0] = time.time()
+    if p.returncode == 0:
+        with open(log_path, 'a') as log_file:
+            writer = csv.writer(log_file)
+            writer.writerow(data_entry)
+    return json.dumps(retval)
 
 
 if __name__ == "__main__":
